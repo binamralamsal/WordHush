f8b3dd5286ac3bab4b5ea3e9d3ea889a67003d65
Binamra Lamsal <binamralamsal@gmail.com>
2025-08-26 16:12:22 +0545

    Update get-user-scores.ts

--- src/services/get-user-scores.ts
+++ src/services/get-user-scores.ts
@@ -53,7 +53,7 @@ export async function getUserScores({
             );
           else
             return eb(
-              "leaderboard.createdAt",
+              sql`date_trunc('year', ${eb.ref("leaderboard.createdAt")})`,
               "=",
               sql<Date>`date_trunc('year', now())`,
             );
